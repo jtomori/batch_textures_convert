af51f37492a222f634f0e74e3c73cac6720d913f
jtomori <tomori.juraj@gmail.com>
2018-05-20 00:59:08 +0200

    started to work on qt widget for the tool

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# batch_textures_convert
-Batch texture conversion to various render-friendly mip-mapped formats
+Batch Textures Conversion
+=========================
+*Batch convert textures to various render-friendly mip-mapped formats*
+
+A few notes
+-----------
+This tool works on Linux and Windows. Feel free to contribute with OS X version. <br>
+Right now the following output formats are supported:
+* .rat - Mantra
+* .tx - Arnold / PRMan
+* .rs - Redshift
+
+However it is easy to extend / modify this tool so that it suits your needs. <br>
+
+To add new output format, simply implement a new class in **houdini/scripts/python/batch_convert.py**, which inherits from **GenericCommand()** class. Class is very simple, so it should be straightforward to add your custom output formats. <br>
+
+This tool relies on external executables to perform conversion (e.g. *iconvert* for *RAT*, *maketx* for *TX*...). Make sure that you have them available in your system's **PATH** variable. If an executable is not found, then it will print a warning and will hide it from the options list.
+
+Installation
+------------
+1. [Download](https://github.com/jtomori/batch_textures_convert/archive/master.zip) or clone this repository.
+
+2. Add **houdini** folder from this repository into your **HOUDINI_PATH** environment variable.
+    * For example add this line into your **houdini.env** file:
+    ```
+    HOUDINI_PATH = &;/path/to/this/repo/houdini
+    ```
+    * If you have set your *HOUDINI_PATH* variable already, then use followig line.
+    ```
+    HOUDINI_PATH = $HOUDINI_PATH;/path/to/this/repo/houdini
+    ```
+
+3. Display **Batch Convert** shelf in Houdini
+
+Usage
+-----
+* Click on **Batch Convert** shelf tool
+* Select which input texture formats should be converted
+    * For example you could convert only jpegs or pngs
+* Select output texture format
+* Confirm
--- houdini/scripts/python/batch_convert.py
+++ houdini/scripts/python/batch_convert.py
@@ -9,6 +9,109 @@ import distutils.spawn
 from Queue import Queue
 from threading import Thread
 
+from PySide2 import QtCore
+from PySide2 import QtWidgets
+
+class Gui(QtWidgets.QWidget):
+    """
+    A class specifying graphical user interface of the tool
+    """
+    def __init__(self, parent=None):
+        super(Gui, self).__init__(parent)
+        
+        self.setParent(hou.ui.mainQtWindow(), QtCore.Qt.Window)
+        self.setProperty("houdiniStyle", True)
+        
+        self.setWindowTitle("Test Gui")
+        self.setMinimumSize(400, 500)
+
+        self.input_formats_list = ["jpg", "png", "tga", "exr"]
+        self.output_formats_list = ["rat", "tx", "rs"]
+
+        # create layouts
+        main_layout = QtWidgets.QVBoxLayout()
+        folder_layout = QtWidgets.QHBoxLayout()
+        form_layout = QtWidgets.QFormLayout()
+        bottom_buttons = QtWidgets.QHBoxLayout()
+
+        # Create widgets
+        file_label = QtWidgets.QLabel("Select a folder with textures for conversion")
+
+        self.folder_path = QtWidgets.QLineEdit()
+        folder_button = hou.qt.createFileChooserButton() # this is H specific
+        folder_button.setFileChooserFilter(hou.fileType.Directory)
+        folder_button.setFileChooserMode(hou.fileChooserMode.Read)
+        folder_button.setFileChooserTitle("Select a folder with textures for conversion")
+        folder_button.setFileChooserStartDirectory( hou.expandString("$JOB") )
+
+        self.input_formats = QtWidgets.QListWidget()
+        self.input_formats.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
+        self.input_formats.addItems(self.input_formats_list)
+        self.input_formats.setFixedHeight( self.input_formats.sizeHintForRow(0) * (self.input_formats.count()+2) )
+        
+        #self.output_format = QtWidgets.QComboBox()
+        self.output_format = hou.qt.createComboBox() # this is H specific
+        self.output_format.addItems(self.output_formats_list)
+        
+        button_convert = QtWidgets.QPushButton("Convert")
+        button_cancel = QtWidgets.QPushButton("Cancel")
+        
+        # Create assign widgets and layouts
+        main_layout.addWidget(file_label)
+        
+        folder_layout.addWidget(self.folder_path)
+        folder_layout.addWidget(folder_button)
+        
+        form_layout.addRow("Input Formats", self.input_formats)
+        form_layout.addRow("Output Format", self.output_format)
+        
+        bottom_buttons.addStretch(1)
+        bottom_buttons.addWidget(button_cancel)
+        bottom_buttons.addWidget(button_convert)
+
+        main_layout.addLayout(folder_layout)
+        main_layout.addLayout(form_layout)
+        main_layout.addStretch(1)
+        main_layout.addLayout(bottom_buttons)
+
+        # Set dialog main_layout
+        self.setLayout(main_layout)
+        
+        # center the window
+        my_dimensions = self.frameGeometry()
+        centerPoint = QtWidgets.QDesktopWidget().availableGeometry().center()
+        my_dimensions.moveCenter(centerPoint)
+        self.move(my_dimensions.topLeft())
+
+        # Add button signals
+        folder_button.fileSelected.connect(self.applyFolderPath)
+        button_convert.clicked.connect(self.convert)
+        button_cancel.clicked.connect(self.cancel)  
+
+    @QtCore.Slot(str)
+    def applyFolderPath(self, path):
+        path = hou.expandString(path)
+        self.folder_path.setText(path)
+
+    def convert(self):
+        """
+        Greets the user
+        """
+        print "convert"
+    
+    def cancel(self):
+        """
+        Cancel button
+        """
+        self.close()
+
+def runGui():
+    """
+    displays the gui
+    """
+    dialog = Gui()
+    dialog.show()
+
 class GenericCommand(object):
     """
     abstract class that should be used as a base for classes implementing conversion for various renderers
@@ -30,7 +133,7 @@ class GenericCommand(object):
         pass
 
     @abc.abstractmethod
-    def generate_command(self):
+    def generateCommand(self):
         """
         should return a list containing command and arguments
         """
@@ -47,7 +150,7 @@ class GenericCommand(object):
         for cmd_class in command_classes:
             found = distutils.spawn.find_executable( cmd_class.executable() )
             if found:
-                command_classes_dict[ cmd_class.name() ] = cmd_class.generate_command
+                command_classes_dict[ cmd_class.name() ] = cmd_class.generateCommand
             else:
                 print( 'Warning: "{executable}" executable was not found, hiding "{format}" option.'.format( executable=cmd_class.executable(), format=cmd_class.name() ) )
         
@@ -67,7 +170,7 @@ class Rat(GenericCommand):
         return "iconvert"
 
     @staticmethod
-    def generate_command(texture_in):
+    def generateCommand(texture_in):
         texture_out = texture_in.split(".")
         texture_out[-1] = "rat"
         texture_out = ".".join(texture_out)
@@ -87,7 +190,7 @@ class Tx(GenericCommand):
         return "maketx"
 
     @staticmethod
-    def generate_command(texture_in):
+    def generateCommand(texture_in):
         texture_out = texture_in.split(".")
         texture_out[-1] = "tx"
         texture_out = ".".join(texture_out)
@@ -107,11 +210,11 @@ class Rs(GenericCommand):
         return "TextureProcessor"
 
     @staticmethod
-    def generate_command(texture_in):
+    def generateCommand(texture_in):
 
         return [Rs.executable(), texture_in]
 
-def worker_convert(q, convert_command):
+def workerConvert(q, convert_command):
     """
     a worker function, which is executed in parallel
     """
@@ -136,7 +239,7 @@ def batchConvert():
 
     # ask user for formats to convert
     img_options = [".jpg", ".jpeg", ".tga", ".exr", ".tif", ".tiff", ".png", ".bmp", ".gif", ".ppm", ".hdr"]
-    img = hou.ui.selectFromList(choices=img_options, default_choices=range( len(img_options) ), message="Select input texture formats to be converted", title="Choose input texture formats", clear_on_cancel=True)    
+    img = hou.ui.selectFromList(choices=img_options, default_choices=range( len(img_options) ), message="Select input texture formats to be converted", title="Choose input texture formats", clear_on_cancel=True, column_header="Formats")    
     if len(img) == 0:
         return
 
@@ -145,7 +248,7 @@ def batchConvert():
     # ask user for output format
     command_classes_dict = GenericCommand.getValidChildCommands()
 
-    command = hou.ui.selectFromList(choices=command_classes_dict.keys(), exclusive=True, default_choices=[0], message="Select output texture format", title="Choose output format", clear_on_cancel=True)
+    command = hou.ui.selectFromList(choices=command_classes_dict.keys(), exclusive=True, default_choices=[0], message="Select output texture format", title="Choose output format", clear_on_cancel=True, column_header="Formats" )
     if len(command) == 0:
         return
     command = command_classes_dict.keys()[ command[0] ]
@@ -182,7 +285,7 @@ def batchConvert():
 
         # spawn threads with convert function
         for i in range(threads):
-            worker = Thread(target=worker_convert, args=(texturesQ, command))
+            worker = Thread(target=workerConvert, args=(texturesQ, command))
             worker.setDaemon(True)
             worker.start()
 
